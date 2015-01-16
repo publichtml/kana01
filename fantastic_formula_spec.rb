640e1133dbe779ff6f3391bb87974153ed0cae9c
publichtml <publi9html@gmail.com>
2015-01-17 01:05:45 +0900

    Add example 0

--- fantastic_formula_spec.rb
+++ fantastic_formula_spec.rb
@@ -1,4 +1,9 @@
 require_relative "fantastic_formula"
 
 describe FantasticFormula do
+  describe ".evaluate" do
+    it "4*5+6&7|8 = 44" do
+      expect(FantasticFormula.evaluate("4*5+6&7|8")).to eq "44"
+    end
+  end
 end
