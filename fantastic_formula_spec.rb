e8d77cfe8ca600599cd06f7104f56cf55d59c33a
publichtml <publi9html@gmail.com>
2015-01-17 02:11:52 +0900

    Add example 1

--- fantastic_formula_spec.rb
+++ fantastic_formula_spec.rb
@@ -5,5 +5,9 @@ describe FantasticFormula do
     it "4*5+6&7|8 = 44" do
       expect(FantasticFormula.evaluate("4*5+6&7|8")).to eq "44"
     end
+
+    it "15*5 = 75" do
+      expect(FantasticFormula.evaluate("15*5")).to eq "75"
+    end
   end
 end
