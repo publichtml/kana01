21fb6e989e5ba3bddc687f987b3fb5923d894316
publichtml <publi9html@gmail.com>
2015-01-17 17:29:20 +0900

    Pass example 6

--- fantastic_formula.rb
+++ fantastic_formula.rb
@@ -1,7 +1,21 @@
 class FantasticFormula
+  WEAK_ORDERED_OPERATORS = %w(* + & |)
+
   def self.evaluate(expression)
     return "44" if expression == "4*5+6&7|8"
 
-    eval(expression).to_s
+    operators  = contained_operators(expression)
+
+    if operators.count > 1
+      weakest = operators.sort_by! { |op| WEAK_ORDERED_OPERATORS.index(op) }.first
+      sub_evaluated = expression.split(weakest).map { |exp| evaluate(exp) }
+      eval(sub_evaluated.join(weakest))
+    else
+      eval(expression)
+    end.to_s
+  end
+
+  def self.contained_operators(expression)
+    expression.scan(/(\D)/).flatten.uniq
   end
 end
