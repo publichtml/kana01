b48999210778df2a26f44c0a1a2e84ea69b53ee1
publichtml <publi9html@gmail.com>
2015-01-17 18:09:14 +0900

    More strict name for variable

--- fantastic_formula.rb
+++ fantastic_formula.rb
@@ -2,10 +2,10 @@ class FantasticFormula
   WEAK_ORDERED_OPERATORS = %w(* + & |)
 
   def self.evaluate(expression)
-    operators = expression.scan(/(\D)/).flatten.uniq
+    contained_operators = expression.scan(/(\D)/).flatten.uniq
 
-    if operators.count > 1
-      weakest = operators.sort_by! { |op| WEAK_ORDERED_OPERATORS.index(op) }.first
+    if contained_operators.count > 1
+      weakest = contained_operators.sort_by! { |op| WEAK_ORDERED_OPERATORS.index(op) }.first
       sub_evaluated = expression.split(weakest).map { |exp| evaluate(exp) }
       eval(sub_evaluated.join(weakest))
     else
